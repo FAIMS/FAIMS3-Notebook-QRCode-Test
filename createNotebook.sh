3d55dc313fb61e4fa38d4a66dfb75ddbf19bf5f5
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2022-11-29 15:05:36 +1100

    editing qrcode test
    Signed-off-by: Brian Ballsun-Stanton <Denubis@users.noreply.github.com>

--- createNotebook.sh
+++ createNotebook.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 export SERVER_FULLNAME="${1:-dev.db.faims.edu.au}" # dev, alpha, testing, selenium
-cp csirogeochem.json faims3-temp-notebook-migrator/
+cp notebook.json faims3-temp-notebook-migrator/
 cd faims3-temp-notebook-migrator
 echo "Load Geochem ${SERVER_FULLNAME}"
 #sed -e "s/##SPID##/${SPID}/g" 20220110-model1.json > fiptemp.json
-bash loadNotebook.sh $SERVER_FULLNAME "CSIRO Geochem $(date)" "csirogeochem.json" "csirogeochem"
-rm -f faims3-temp-notebook-migrator/csirogeochem.json
+bash loadNotebook.sh $SERVER_FULLNAME "QRCode Test $(date)" "notebook.json" "qrcode-test"
+rm -f faims3-temp-notebook-migrator/notebook.json
 rm -f faims3-temp-notebook-migrator/testnotebook.json
